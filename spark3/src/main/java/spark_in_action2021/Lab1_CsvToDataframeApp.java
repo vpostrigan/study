134d316f9607c70e0ca79a06c988c0d34dbfcb05
vpostrigan <postriganvova@gmail.com>
2021-12-20 14:55:33 +0200

    Spark study

---
+++ spark3/src/main/java/spark_in_action2021/Lab1_CsvToDataframeApp.java
@@ -0,0 +1,36 @@
+package spark_in_action2021;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * https://github.com/jgperrin/net.jgp.books.spark.ch01
+ */
+public class Lab1_CsvToDataframeApp {
+
+    public static void main(String[] args) {
+        Lab1_CsvToDataframeApp app = new Lab1_CsvToDataframeApp();
+        app.start();
+    }
+
+    /**
+     * The processing code.
+     */
+    private void start() {
+        // Creates a session on a local master
+        SparkSession spark = SparkSession.builder()
+                .appName("CSV to Dataset")
+                .master("local")
+                .getOrCreate();
+
+        // Reads a CSV file with header, called books.csv, stores it in a dataframe
+        Dataset<Row> df = spark.read().format("csv")
+                .option("header", "true")
+                .load("data/books.csv");
+
+        // Shows at most 5 rows from the dataframe
+        df.show(5);
+    }
+
+}
---
+++ spark3/src/main/java/spark_in_action2021/Lab2.java
@@ -0,0 +1,7 @@
+package spark_in_action2021;
+
+/**
+ * https://github.com/jgperrin/net.jgp.books.spark.ch02
+ */
+public class Lab2 {
+}
