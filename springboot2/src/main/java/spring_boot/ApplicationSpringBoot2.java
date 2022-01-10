ece308191dfa7fa2b6dbadef4d95729426b72d69
vpostrigan <postriganvova@gmail.com>
2022-01-10 14:44:10 +0200

    New SpringBoot crud application

---
+++ springboot2/src/main/java/spring_boot/ApplicationSpringBoot2.java
@@ -0,0 +1,13 @@
+package spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSpringBoot2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSpringBoot2.class, args);
+    }
+
+}
