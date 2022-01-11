f84e1cf37bd54a754d5fe8a2617bcc0c8cadf032
vpostrigan <postriganvova@gmail.com>
2022-01-11 23:10:04 +0200

    SpringBoot REST app

--- springboot2/src/main/java/spring_boot/ApplicationSpringBoot2.java
+++ springboot2/src/main/java/spring_boot/ApplicationSpringBoot2.java
@@ -1,8 +1,15 @@
 package spring_boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+@SpringBootApplication(scanBasePackages = {
+        "spring_boot.entity",
+        "spring_boot.repository",
+        "spring_boot.service",
+        "spring_boot.service.impl",
+        "spring_boot.controller" })*/
 @SpringBootApplication
 public class ApplicationSpringBoot2 {
 
